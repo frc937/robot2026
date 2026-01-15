0e5153590b5cd2c41077d0565b5f62fece11a1c5
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2026-01-15 09:06:49 -0600

    layout skeleton of constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,5 +4,53 @@
 
 package frc.robot;
 
-/** Add your docs here. */
-public class Constants {}
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+    /** Constants relating to controllers */
+    public static final class Controllers {
+
+    }
+
+    /** Constants for the drivetrain. */
+    public static final class Drive {
+
+    }
+
+    /** Constants relating to autos. */
+    public static final class Auto {
+
+    }
+
+    /** Constants for the Climber system */
+    public static final class Climber {
+
+    }
+
+    /** Constants for the Intake system. */
+    public static final class Intake {
+
+    }
+
+    /** Constants for the output system. */
+    public static final class Shooter {
+
+    }
+
+    /** Constants for the Limelight. */
+    public static final class Limelight {
+
+    }
+
+    /** Constants for the USB cameras. */
+    public static final class Camera {
+
+    }
+}
