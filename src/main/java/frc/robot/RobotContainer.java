e96eb3d8b5cb187816112600b642de3fefbe885a
pneumatic-Peanuts <simonkendle7@gmail.com>
2026-01-22 14:56:36 -0600

    format robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,25 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotContainer {
+  
+  /*
+   * **************
+   * * SUBSYSTEMS *
+   * **************
+   */
+  
+
+  /*
+   * ************
+   * * COMMANDS *
+   * ************
+   */
+
+  /*
+   * ***********************
+   * * OTHER INSTANCE VARS *
+   * ***********************
+   */
   public RobotContainer() {
     configureBindings();
   }
