e3aaa8ba5df8b20b61b7461a1ba48e8c766e1966
fluffhead921 <routhmaya@gmail.com>
2026-03-10 16:08:49 -0500

    Created ClimberSubsystem skeleton. Skeleton methods for running and stopping climber mechanisms.

---
+++ src/main/java/frc/robot/subsystem/ClimberSubsystem.java
@@ -0,0 +1,47 @@
+package frc.robot.subsystem;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClimberSubsystem extends SubsystemBase {
+    
+    public ClimberSubsystem() {
+
+    }
+
+    public void runClimberInABox() {
+
+    }
+
+    public void runClimberInABox(double speed) {
+
+    }
+
+    public void runChainClimber() {
+
+    }
+
+    public void runChainClimber(double speed) {
+
+    }
+
+    public void reverseClimberInABox() {
+
+    }
+
+    public void reverseChainClimber() {
+
+    }
+
+    public void stopClimberInABox() {
+
+    }
+
+    public void stopChainClimber() {
+
+    }
+
+    public void stop() {
+
+    }
+
+}
