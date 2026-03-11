f92e237763bf8f66194cb1a65b2f6e94fc4a6d5e
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2026-03-11 17:31:49 -0500

    javadoc and reformatting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,15 +15,23 @@ import frc.robot.command.DriveRobot;
 import frc.robot.command.ToggleFieldRelativity;
 import frc.robot.subsystem.Drive;
 import frc.robot.subsystem.Shooter;
+
+
 /** Singleton class that contains all the robot's subsystems, commmands, and button bindings. */
 public class RobotContainer {
+
+  /*
+   * ***********************
+   * * OTHER INSTANCE VARS *
+   * ***********************
+   */
   
    /**Singleton instance of the Driver controller for the whole robot. */
   public static CommandXboxController pilotController = new CommandXboxController(Controllers.PILOT_CONTROLLER_PORT);
 
    /**Singleton instance of the Operator controller for the whole robot. */
   public static CommandXboxController operatorController = new CommandXboxController(Controllers.OPERATOR_CONTROLLER_PORT);
-   /** Constructer for the {@link RobotContainer} */
+   
 
 
 
@@ -34,12 +42,14 @@ public class RobotContainer {
    */
 
 
-  /** SIngleton instance of the {@link Drive} for the whole robot. */
+  /** Singleton instance of the {@link Drive} for the whole robot. */
   public static Drive drivebase = new Drive(new File(Filesystem.getDeployDirectory(), "swerve"));
 
 
-   /** Singleton instance of the {@link Shooter} for the whole robot. */
-   public static Shooter shooter = new Shooter();
+  /** Singleton instance of the {@link Shooter} for the whole robot. */
+  public static Shooter shooter = new Shooter();
+
+
 
   /*
    * ************
@@ -47,43 +57,51 @@ public class RobotContainer {
    * ************
    */
 
-   /** singleton instance of the robot oriented {@link DriveRobot} command for the whole robot. */
+  /** Singleton instance of the robot oriented {@link DriveRobot} command for the whole robot. */
   public static DriveRobot driveRobotOriented = new DriveRobot(drivebase, pilotController, false);
 
-  /** singleton instance of the field oriented {@link DriveRobot} command for the whole robot. */
+  /** Singleton instance of the field oriented {@link DriveRobot} command for the whole robot. */
   public static DriveRobot driveFieldOriented = new DriveRobot(drivebase, pilotController, true);
 
+
+
+  /** Singleton instance of the field oriented {@link ToggleFieldRelativity} command for the whole robot */
   public static ToggleFieldRelativity toggleFieldRelativity = new ToggleFieldRelativity(
     drivebase, 
     driveRobotOriented,
-    driveFieldOriented);
-
-  /*
-   * ***********************
-   * * OTHER INSTANCE VARS *
-   * ***********************
-   */
+    driveFieldOriented
+  );
 
 
+
+
+
+
+  /** Constructer for the {@link RobotContainer} */
   public RobotContainer() {
     configureBindings();
 
     drivebase.setDefaultCommand(driveRobotOriented);
   }
   
+
   private void configureBindings() {
+    /* Operator bindings. */
     operatorController.leftTrigger().whileTrue(Commands.run(shooter::runIntake, shooter).finallyDo(shooter::stop));
     operatorController.rightTrigger().whileTrue(Commands.run(shooter::runShooter, shooter).finallyDo(shooter::stop));
     operatorController.b().whileTrue(Commands.run(shooter::runIntakeReverse, shooter).finallyDo(shooter::stop));
 
+    /* Pilot bindings. */
     pilotController.b().whileTrue(Commands.run(drivebase::lock, drivebase));
     pilotController.leftStick().onTrue(toggleFieldRelativity);
     
   }
-/**Get the current autonomus command.
- * 
- * @return The current autonomus command.
- */
+
+  /**
+  * Get the current autonomus command.
+  * 
+  * @return The current autonomus command.
+  */
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
