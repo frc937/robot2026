88af9960cabbf3701b4332c42e6ac01f021a18be
fluffhead921 <routhmaya@gmail.com>
2026-03-12 11:46:28 -0500

    added JavaDocs for existing climber mechanism methods.

--- src/main/java/frc/robot/subsystem/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystem/ClimberSubsystem.java
@@ -34,38 +34,53 @@ public class ClimberSubsystem extends SubsystemBase {
         chainMotor.configure(chainConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
     }
 
+    /**Run climber-in-a-box motors at speed specified in {@link ClimberConstants}} */
     public void runClimberInABox() {
         ciabLeaderMotor.set(ClimberConstants.CIAB_SPEED);
     }
 
+    /**Run climber-in-a-box motors at provided speed
+     * 
+     * @param speed Motor speed between -1.0 and 1.0
+     */
     public void runClimberInABox(double speed) {
         ciabLeaderMotor.set(speed);
     }
 
+    /**Run chain climber motor at speed specified in {@link ClimberConstants}} */
     public void runChainClimber() {
         chainMotor.set(ClimberConstants.CHAIN_SPEED);
     }
 
+    /**Run chain climber motor at provided speed
+     * 
+     * @param speed Motor speed between -1.0 and 1.0
+     */
     public void runChainClimber(double speed) {
         chainMotor.set(speed);
     }
 
+    /**Run climber-in-a-box motors in reverse at speed specified in {@link ClimberConstants}} */
     public void reverseClimberInABox() {
         ciabLeaderMotor.set(-ClimberConstants.CIAB_SPEED);
     }
 
+    /**Run chain climber motor in reverse at speed specified in {@link ClimberConstants}} */
     public void reverseChainClimber() {
         chainMotor.set(-ClimberConstants.CHAIN_SPEED);
     }
 
+    /**Stop the climber-in-a-box motors */
     public void stopClimberInABox() {
         ciabLeaderMotor.stopMotor();
     }
 
+    /**Stop the chain climber motor */
     public void stopChainClimber() {
         chainMotor.stopMotor();
     }
 
+    /**Stop all climber motors */
     public void stop() {
         ciabLeaderMotor.stopMotor();
         chainMotor.stopMotor();
