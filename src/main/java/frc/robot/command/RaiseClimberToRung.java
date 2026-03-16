28465289230d853c14c9195264cb76339c1ccae7
fluffhead921 <routhmaya@gmail.com>
2026-03-16 11:54:35 -0500

    removed redundancy of isFinished condition

--- src/main/java/frc/robot/command/RaiseClimberToRung.java
+++ src/main/java/frc/robot/command/RaiseClimberToRung.java
@@ -6,7 +6,6 @@ import frc.robot.subsystem.ClimberSubsystem;
 public class RaiseClimberToRung extends Command {
 
     private ClimberSubsystem climber;
-    private boolean isFinished = false;
 
     public RaiseClimberToRung(ClimberSubsystem climber) {
         this.climber = climber;
@@ -19,20 +18,13 @@ public class RaiseClimberToRung extends Command {
     }   
 
     @Override
-    public void execute() {
-        if (climber.isClimbReady()) {
-            isFinished = true;
-        }
-    }
-
-    @Override
     public void end(boolean interrupted) {
         climber.stopClimberInABox();
     }
 
     @Override
     public boolean isFinished() {
-        return isFinished;
+        return climber.isClimbReady();
     }
 
 }
