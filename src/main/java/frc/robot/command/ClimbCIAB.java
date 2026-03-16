3fe7932d330dd081a2ec46e0d116b8121052ddca
fluffhead921 <routhmaya@gmail.com>
2026-03-16 11:47:01 -0500

    created ClimbCIAB command skeleton. will lower climber in a box to pull robot up.

---
+++ src/main/java/frc/robot/command/ClimbCIAB.java
@@ -0,0 +1,38 @@
+package frc.robot.command;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystem.ClimberSubsystem;
+
+public class ClimbCIAB extends Command {
+
+    private ClimberSubsystem climber;
+
+    public ClimbCIAB(ClimberSubsystem climber) {
+        this.climber = climber;
+        addRequirements(climber);
+    }
+
+    @Override
+    public void initialize() {
+        // TODO Auto-generated method stub
+        super.initialize();
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        super.execute();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        // TODO Auto-generated method stub
+        super.end(interrupted);
+    }
+
+    @Override
+    public boolean isFinished() {
+        // TODO Auto-generated method stub
+        return super.isFinished();
+    }
+}
