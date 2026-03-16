a1f929a9f76f8cad9eb97ac0de7b13a2c49c15b3
fluffhead921 <routhmaya@gmail.com>
2026-03-16 11:57:33 -0500

    changed name of ClimbCIAB to LowerCIAB

--- src/main/java/frc/robot/command/LowerCIAB.java
+++ src/main/java/frc/robot/command/LowerCIAB.java
@@ -3,11 +3,11 @@ package frc.robot.command;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystem.ClimberSubsystem;
 
-public class ClimbCIAB extends Command {
+public class LowerCIAB extends Command {
 
     private ClimberSubsystem climber;
 
-    public ClimbCIAB(ClimberSubsystem climber) {
+    public LowerCIAB(ClimberSubsystem climber) {
         this.climber = climber;
         addRequirements(climber);
     }
