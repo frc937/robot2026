5c997f28fa13d118b8c2a649378bd713ed394515
fluffhead921 <routhmaya@gmail.com>
2026-03-16 13:15:26 -0500

    command to lower climber in a box until IR Sensor detects rung. modified from RaiseCIABToRung.

---
+++ src/main/java/frc/robot/command/LowerCIABToRung.java
@@ -0,0 +1,30 @@
+package frc.robot.command;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystem.ClimberSubsystem;
+
+public class LowerCIABToRung extends Command {
+
+    private ClimberSubsystem climber;
+
+    public LowerCIABToRung(ClimberSubsystem climber) {
+        this.climber = climber;
+        addRequirements(climber);
+    }
+
+    @Override
+    public void initialize() {
+        climber.reverseClimberInABox();
+    }   
+
+    @Override
+    public void end(boolean interrupted) {
+        climber.stopClimberInABox();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return climber.isClimbReady();
+    }
+
+}
